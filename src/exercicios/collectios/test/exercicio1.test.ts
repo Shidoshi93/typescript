214d88228c5a2ed7d6f201ce7ba0bd9acdcce81d
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-11-04 23:34:06 -0300

    test: adds unit test

---
+++ src/exercicios/collectios/test/exercicio1.test.ts
@@ -0,0 +1,15 @@
+import type { cores } from "../exercicio1.ts";
+import { processarCores } from '../exercicio1.ts';
+
+const esperado: Array<String> = ['branco', 'rosa', 'verde', 'laranja', 'preto'];
+const resultado: cores = {
+    "cores": ["branco", "rosa", "verde", "laranja", "preto"], 
+    "coresOrdenadas": ["branco", "laranja", "preto", "rosa", "verde"]
+};
+
+describe('Exercicio 1', () => {
+    test(`Deve retornar ${resultado.coresOrdenadas}`, () => {
+        console.log("entrou no teste")
+        expect(processarCores(esperado)).toEqual(resultado);
+    })
+})
